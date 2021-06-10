dd982e297c302a153dc876b6bf3fc702cdbe7d9e
bwkw <na.shota.1139@gmail.com>
2021-06-10 21:22:36 +0900

    カレンダー登録ページ追加

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/register', function () {
+    return view('Calendar.register');
+});
