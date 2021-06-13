7163e0d22df55eabe3ec2f3db39e567ad54ca369
bwkw <na.shota.1139@gmail.com>
2021-06-13 23:39:13 +0900

    ホーム画面への遷移を追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,21 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    //ログイン認証
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+    //ホーム
     public function index()
     {
         return view('Home.home');
