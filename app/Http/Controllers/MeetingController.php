462e0ef03d1c448388b4036ee4972fa2a9380a27
bwkw <na.shota.1139@gmail.com>
2021-06-13 23:40:06 +0900

    面談日時をmeetingsテーブルに保存するstoreメソッド定義

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Meeting;
 use Illuminate\Http\Request;
 
 class MeetingController extends Controller
 {
-    //
+    //ログイン認証
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    //meetingsテーブルに予定を格納
+    public function store(Meeting $meeting, Request $request)
+    {
+        $input_meeting = $request['meeting'];
+        $meeting->fill($input_meeting)->save();
+        return redirect('/');
+    }
 }
