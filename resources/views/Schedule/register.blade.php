8f3a1f9818df9d4681f572a1c814315008b7b3c9
bwkw <na.shota.1139@gmail.com>
2021-06-13 23:43:07 +0900

    面談登録フォーム作成

--- resources/views/Schedule/register.blade.php
+++ resources/views/Schedule/register.blade.php
@@ -1,169 +1,104 @@
-@extends('layouts.no_app')
-
-@section('register')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-       
-        
-    
-      
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js" 
-        integrity="sha256-AdQN98MVZs44Eq2yTwtoKufhnU+uZ7v2kXnD5vqzZVo=" crossorigin="anonymous"></script>
-    
-    
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/js/tempusdominus-bootstrap-4.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/css/tempusdominus-bootstrap-4.min.css" />
-    
-    
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js" 
-        integrity="sha256-MAgcygDRahs+F/Nk5Vz387whB4kSK9NXlDN3w58LLq0=" crossorigin="anonymous"></script>
-        
-        <style type="text/css">
-            .datepicker-days th.dow:first-child,
-            .datepicker-days td:first-child {
-                color: #f00;
-            }
-            .datepicker-days th.dow:last-child,
-            .datepicker-days td:last-child {
-                color: #00f;
-            }
-        </style>
-        
     </head>
     
     <body>
-        <div>
-            
-            <!-- 切り替えタブ部分 -->
-            <ul class="nav nav-tabs nav-justified mt-3" role="tablist">
-                <!-- 1つ目のタブ -->
-                <li class="nav-item">
-                    <a class="nav-link active" href="#meeting"
-                        data-toggle="tab" role="tab">面談</a>
-                </li>
-                <!-- 2つ目のタブ -->
-                <li class="nav-item">
-                    <a class="nav-link" href="#event" 
-                    data-toggle="tab" role="tab">イベント</a>
-                </li>
-            </ul>
-            
-            <div class="tab-content mt-4 mb-3">
-                <div class="tab-pane fade show active" id="meeting" role="tabpanel">
-                    <div class="row">
-                      
-                        <div class="col-md-1">
-                        </div>
-                        
-                        <div class="col-md-4 border">
-                            <br>
-                            <h2>面談予約</h2>
-                            <br>
-                            <div class="row">
-                                <div class="col-md">
-                                    <form action="/meeting" method="POST">
-                                        <div class="form-group mb-4">
-                                            <label>氏名：</label>
-                                            <input type="text" class="form-control">
-                                        </div>
-                                        <div class="form-group mb-4">
-                                            <label>生徒氏名：</label>
-                                            <input type="text" class="form-control">
-                                        </div>
-                                        <div class="form-group mb-4">
-                                            <label>日程：</label>
-                                            <div class="input-group date" id="datePicker" data-target-input="nearest">
-                                                <input type="text" class="form-control datetimepicker-input" data-target="#datePicker"/>
-                                                <div class="input-group-append" data-target="#datePicker" data-toggle="datetimepicker">
-                                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                                </div>
+        @extends('layouts.app')
+
+        @section('register')
+            <div>
+                <!-- 切り替えタブ部分 -->
+                <ul class="nav nav-tabs nav-justified mt-3" role="tablist">
+                    <!-- 1つ目のタブ -->
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#meeting"
+                            data-toggle="tab" role="tab">面談</a>
+                    </li>
+                    <!-- 2つ目のタブ -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="#event" 
+                        data-toggle="tab" role="tab">イベント</a>
+                    </li>
+                </ul>
+                
+                <div class="tab-content mt-4 mb-3">
+                    <div class="tab-pane fade show active" id="meeting" role="tabpanel">
+                        <div class="row">
+                          
+                            <div class="col-md-1">
+                            </div>
+                            
+                            <div class="col-md-4 border">
+                                <br>
+                                <h2>面談予約</h2>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md">
+                                        <form action="/meetings" method="POST">
+                                            {{ csrf_field() }}
+                                            <div class="form-group mb-4">
+                                                <label>メンター氏名：</label>
+                                                <input type="text" class="form-control" name="meeting[mentor_name]">
                                             </div>
-                                        </div>
-                                        <div class="form-group mb-4">
-                                            <label>開始時間：</label>
-                                            <div class="input-group date" id="timePicker1" data-target-input="nearest">
-                                                <input type="text" class="form-control datetimepicker-input" data-target="#timePicker1"/>
-                                                <div class="input-group-append" data-target="#timePicker1" data-toggle="datetimepicker">
-                                                    <div class="input-group-text"><i class="fa fa-clock"></i></div>
-                                                </div>
+                                            <div class="form-group mb-4">
+                                                <label>生徒氏名：</label>
+                                                <input type="text" class="form-control" name="meeting[student_name]" >
                                             </div>
-                                        </div>
-                                        <div class="form-group mb-4">
-                                            <label>終了時間：</label>
-                                            <div class="input-group date" id="timePicker2" data-target-input="nearest">
-                                                <input type="text" class="form-control datetimepicker-input" data-target="#timePicker2"/>
-                                                <div class="input-group-append" data-target="#timePicker2" data-toggle="datetimepicker">
-                                                    <div class="input-group-text"><i class="fa fa-clock"></i></div>
-                                                </div>
+                                            <div class="form-group mb-4">
+                                                <label for="date" class="col-form-label">日程</label>
+                                                <input type="date" class="form-control" name="meeting[date]" id="date">
                                             </div>
-                                        </div>
-                                        <div class="text-center mt-5">
-                                            <button type="button" class="btn btn-secondary">送信</button>
-                                        </div>
-                                    </form>
+                                            <div class="form-group mb-4">
+                                                <label for="starting_time" class="col-form-label">開始時間</label>
+                                                <input type="time" class="form-control" name="meeting[starting_time]" id="starting_time">
+                                            </div>
+                                            <div class="form-group mb-4">
+                                                <label for="ending_time" class="col-form-label">終了時間</label>
+                                                <input type="time" class="form-control" name="meeting[ending_time]" id="ending_time">
+                                            </div>
+                                            
+                                            <!-- ユーザーidをリレーション用に格納 -->
+                                            <input type="hidden" name="meeting[user_id]" value="{{ Auth::user()->id }}">
+                                            
+                                            <div class="text-center mt-4">
+                                                <button type="submit" class="btn btn-secondary">送信</button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
+                            
+                            <div class="col-md-1">
+                            </div>
+                              
+                            <div class="col-md-5" id="calendar">
+                                    
+                            </div>
+                            
                         </div>
-                        
-                        <div class="col-md-1">
-                        </div>
-                          
-                        <div class="col-md-5" id="calendar">
-                                
-                        </div>
-                        
+                      </div>
+    
+                    <div class="tab-pane fade" id="event"
+                         role="tabpanel">
+                        さようなら
                     </div>
-                  </div>
-
-                <div class="tab-pane fade" id="event"
-                     role="tabpanel">
-                    さようなら
                 </div>
             </div>
-        </div>
-        
-        <script src="{{ mix('js/Calendar/TimeGridView.js') }}"></script>
-      
-        <script type="text/javascript">
-            $(function ()
-                {
-                    
-                    $('#datePicker').datetimepicker({
-                        dayViewHeaderFormat: 'YYYY年 MM月',
-                        format: 'YYYY/MM/DD',
-                        locale: 'ja',
-                    });
-                    
-                    $('#timePicker1').datetimepicker({
-                        format: 'HH:mm',
-                        locale: 'ja',
-                    });
-                    
-                    $('#timePicker2').datetimepicker({
-                        format: 'HH:mm',
-                        locale: 'ja',
-                    });
-                
-                }
-            );
-        </script>
+            
+            <script src="{{ mix('js/Calendar/TimeGridView.js') }}"></script>
+          
+        @endsection
+
+        @section('back')
+            <div class="col text-center mt-4 mb-4">
+                <a href="/">
+                    <button type="button" class="btn btn-secondary">戻る</button>
+                </a>
+            </div>
+        @endsection
       
     </body>
 </html>
-@endsection
-
-@section('back')
-<div class="col text-center mt-4 mb-4">
-    <a href="/">
-        <button type="button" class="btn btn-secondary">戻る</button>
-    </a>
-</div>
-@endsection
