505cf5850de74e5d1ee1a757b22a8fba78472308
bwkw <na.shota.1139@gmail.com>
2021-06-13 23:41:45 +0900

    initial commit

--- database/migrations/2021_06_13_212805_create_meetings_table.php
+++ database/migrations/2021_06_13_212805_create_meetings_table.php
@@ -15,9 +15,9 @@ class CreateMeetingsTable extends Migration
     {
         Schema::create('meetings', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->date('date');
             $table->string('mentor_name');
             $table->string('student_name');
+            $table->date('date');
             $table->time('starting_time');
             $table->time('ending_time');
             $table->integer('user_id');
