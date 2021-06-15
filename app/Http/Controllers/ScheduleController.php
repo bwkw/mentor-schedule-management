458cb70e14204f18bc700e982f9a901f4a5425bb
bwkw <na.shota.1139@gmail.com>
2021-06-15 20:19:20 +0900

    メンターと生徒の名前を取得できるように

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mentor;
+use App\Models\Student;
+
 use Illuminate\Http\Request;
+
 
 class ScheduleController extends Controller
 {
-    //ログイン認証
+    // ログイン認証
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    //スケジュール（ミーティング/イベント）登録ページ
+    // スケジュール（ミーティング/イベント）登録ページ
     public function register()
     {
-        return view('Schedule.register');
+        // メンターと生徒の名前をそれぞれのテーブルから取得
+        $mentors = \DB::table('mentors')->get();
+        $students = \DB::table('students')->get();
+        return view('Schedule.register') -> with(
+            [
+                'mentors' => $mentors,
+                'students' => $students
+            ]
+            );
     }
 }
