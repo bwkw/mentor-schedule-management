83a87063b00d199e95dce085488d203fdf029ff8
bwkw <na.shota.1139@gmail.com>
2021-06-16 17:16:31 +0900

    日時登録を追加

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EventController extends Controller
 {
-    //
+    // ログイン認証
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    // ユーザー情報に紐づいてeventsテーブルのデータを表示する
+    public function index()
+    {
+        $your_events = User::find(Auth::user()->id)->events;
+        return $your_events;
+    }
+    
+    // eventsテーブルに予定を格納
+    public function store(Event $event, Request $request)
+    {
+        $input_event = $request['event'];
+        $event->fill($input_event)->save();
+        return redirect('/');
+    }
+    
+    // ミーティング登録ページ
+    public function register()
+    {
+        // メンターと生徒の名前をそれぞれのテーブルから取得
+        $mentors = \DB::table('mentors')->get();
+        $students = \DB::table('students')->get();
+        return view('Meeting.register') -> with(
+            [
+                'mentors' => $mentors,
+                'students' => $students,
+            ]
+        );
+    }
 }
--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -2,31 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Meeting;
 use App\Models\User;
+use App\Models\Meeting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MeetingController extends Controller
 {
-    //ログイン認証
+    // ログイン認証
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    //ユーザー情報に紐づいてmeetingsテーブルのデータを表示する
+    // ユーザー情報に紐づいてmeetingsテーブルのデータを表示する
     public function index()
     {
         $your_meetings = User::find(Auth::user()->id)->meetings;
         return $your_meetings;
     }
     
-    //meetingsテーブルに予定を格納
+    // meetingsテーブルに予定を格納
     public function store(Meeting $meeting, Request $request)
     {
         $input_meeting = $request['meeting'];
         $meeting->fill($input_meeting)->save();
         return redirect('/');
+    }
+    
+    // ミーティング日時登録ページ
+    public function register()
+    {
+        // メンターと生徒の名前をそれぞれのテーブルから取得
+        $mentors = \DB::table('mentors')->get();
+        $students = \DB::table('students')->get();
+        return view('Meeting.register') -> with(
+            [
+                'mentors' => $mentors,
+                'students' => $students,
+            ]
+        );
     }
 }
