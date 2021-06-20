c520be5a1f7d80df0bb55d4495fa36645b3699c4
bwkw <na.shota.1139@gmail.com>
2021-06-20 11:28:22 +0900

    面談とイベントの表示をプロパティによって条件分岐

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,24 @@ class HomeController extends Controller
     //ホーム
     public function index()
     {
+        $meetings = \DB::table('meetings')->get();
+        $events = \DB::table('events')->get();
+        // 二つのオブジェクトを結合する
+        $meetings_events = (object)array_merge_recursive((array) $meetings, (array) $events);
         
-        return view('Home.home');
+        // オブジェクトを配列に
+        $meetings_events_array = "";
+        foreach($meetings_events as $meeting_event)
+        {
+            $meetings_events_array=$meeting_event;
+        }
+        
+        // 配列からdateとstarting_timeを取得し、日時で昇順に並び替える
+        $dates = array_column($meetings_events_array, 'date');
+        $starting_times = array_column($meetings_events_array, 'starting_time');
+        array_multisort($dates, SORT_DESC, $meetings_events_array);
+        array_multisort($starting_times, SORT_DESC, $meetings_events_array);
+        //dd($meetings_events_array);
+        return view('Home.home') -> with(['meetings_events_array' => $meetings_events_array]);
     }
 }
