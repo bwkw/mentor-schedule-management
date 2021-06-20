d2baba3d00c5c31f843ec2d1cc3b06e188853103
bwkw <na.shota.1139@gmail.com>
2021-06-20 11:30:38 +0900

    面談とイベントの表示をプロパティによって条件分岐

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -9,9 +9,38 @@
             <div class="col-md-1">
             </div>
             
-            <div class="col-md-4">
-                <h3><a href="/meetings/register">面談の登録</a></h3>
-                <h3><a href="/events/register">イベントの登録</a></h3>
+            <div class="col-md-4 border">
+                <div class="row justify-content-center mt-3">
+                    <a href="/meetings/register">
+                        <button type="button" class="btn btn-primary btn-lg">面談の登録</button>
+                    </a>
+                    <div class="col-md-1"></div>
+                    <a href="/events/register">
+                        <button type="button" class="btn btn-warning btn-lg">イベントの登録</button>
+                    </a>
+                </div>
+                
+                <div>
+                    <div class="card mt-4" style="width: 18rem;">
+                        <div class="card-header">
+                            今後の予定
+                        </div>
+                        // 面談とイベントの表示をプロパティによって条件分岐
+                        @foreach($meetings_events_array as $key => $meeting_event)
+                            @if(!empty(($meeting_event->student_name)))
+                                <li class="list-group-item">
+                                    {{$meeting_event->date}}：{{$meeting_event->starting_time}}〜{{$meeting_event->ending_time}}<br>
+                                    {{$meeting_event->student_name}}との面談<br>
+                                </li>
+                            @else
+                                <li class="list-group-item">
+                                    {{$meeting_event->date}}：{{$meeting_event->starting_time}}〜{{$meeting_event->ending_time}}<br>
+                                    {{$meeting_event->event_name}}<br>
+                                </li>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
             </div>
             
             <div class="col-md-1">
@@ -29,7 +58,7 @@
     <div class="modal modal-background">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">面談予約</h5>
+                <h5 class="modal-title">イベント情報</h5>
             </div>
             <div class="modal-body">
           　    <div class="modal-body-start-time"></div>
