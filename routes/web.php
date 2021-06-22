b17bb4cad3b0f1513626c6e755f7b1c52bf86857
bwkw <na.shota.1139@gmail.com>
2021-06-22 20:00:36 +0900

    編集用のルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,23 @@ Auth::routes();
 // ホーム
 Route::get('/', 'HomeController@index');
 
-// ミーティング日時を取得
+// 面談日時を取得
 Route::get('/meetings', 'MeetingController@index');
 
-// ミーティング日時を保存
+// 面談日時を保存
 Route::post('/meetings', 'MeetingController@store');
 
-// ミーティング日時登録ページ
+// 面談日時登録ページ
 Route::get('/meetings/register', 'MeetingController@register');
 
-// ミーティング日時削除
+// 面談日時を削除
 Route::delete('/meetings/{meeting}', 'MeetingController@delete');
+
+// 面談日時の編集ページへ
+Route::get('/meetings/{meeting}/edit', 'MeetingController@edit');
+
+// 面談日時の編集/更新
+Route::put('/meetings/{meeting}', 'MeetingController@update');
 
 // イベント日時を取得
 Route::get('/events', 'EventController@index');
@@ -36,3 +42,12 @@ Route::post('/events', 'EventController@store');
 
 // イベント登録ページ
 Route::get('/events/register', 'EventController@register');
+
+// イベント日時を削除
+Route::delete('/events/{event}', 'EventController@delete');
+
+// イベント日時の編集ページへ
+Route::get('/events/{event}/edit', 'EventController@edit');
+
+// イベント日時の編集/更新
+Route::put('/events/{event}', 'EventController@update');
