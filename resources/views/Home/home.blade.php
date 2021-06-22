98f387c8700a8586ed766bead14beae77142e7d1
bwkw <na.shota.1139@gmail.com>
2021-06-22 18:47:29 +0900

    変更なし

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -6,10 +6,12 @@
 
     <div>
         <div class="row mt-5">
+            
             <div class="col-md-1">
             </div>
             
             <div class="col-md-4 border">
+                
                 <div class="row justify-content-center mt-3">
                     <a href="/meetings/register">
                         <button type="button" class="btn btn-primary btn-lg">面談の登録</button>
@@ -21,25 +23,63 @@
                 </div>
                 
                 <div>
-                    <div class="card mt-4" style="width: 18rem;">
+                    <div class="col-md-1"></div>
+                    
+                    <div class="card mt-4" >
                         <div class="card-header">
                             今後の予定
                         </div>
-                        // 面談とイベントの表示をプロパティによって条件分岐
-                        @foreach($meetings_events_array as $key => $meeting_event)
-                            @if(!empty(($meeting_event->student_name)))
-                                <li class="list-group-item">
-                                    {{$meeting_event->date}}：{{$meeting_event->starting_time}}〜{{$meeting_event->ending_time}}<br>
-                                    {{$meeting_event->student_name}}との面談<br>
+                        <ul class="list-group list-group-flush">
+                            
+                            @if( count( $meetings_events_array )==0 )
+                                <li class="list-group-item d-flex justify-content-around align-items-center">
+                                    現在予定はありません
                                 </li>
                             @else
-                                <li class="list-group-item">
-                                    {{$meeting_event->date}}：{{$meeting_event->starting_time}}〜{{$meeting_event->ending_time}}<br>
-                                    {{$meeting_event->event_name}}<br>
-                                </li>
+                                <!-- 面談とイベントの表示をプロパティによって条件分岐 -->
+                                @foreach( $meetings_events_array as $key => $meeting_event )
+                                    @if( !empty( $meeting_event->student_name ) )
+                                    
+                                        <li class="list-group-item d-flex justify-content-around align-items-center">
+                                            {{ $meeting_event->date }}：{{ $meeting_event->beginning_time }}〜{{$meeting_event->ending_time}}<br>
+                                            {{ $meeting_event->student_name }}との面談
+                                            
+                                            <form action="/meetings/{{ $meeting_event->id }}" id="form_delete" method="post">
+                                                {{ csrf_field() }}
+                                                @method('DELETE')
+                                                <button type="button" class="btn btn-secondary" onclick="deleteSchedule(this);">削除</button> 
+                                            </form>
+                                            
+                                            <a href="/meetings/{{ $meeting_event->id }}/edit">
+                                                <button type="button" class="btn btn-secondary">編集</button>
+                                            </a>
+                                        </li>
+                                        
+                                    @else
+                                    
+                                        <li class="list-group-item d-flex justify-content-around align-items-center">
+                                            {{ $meeting_event->date }}：{{ $meeting_event->beginning_time }}〜{{ $meeting_event->ending_time }}<br>
+                                            {{ $meeting_event->event_name }}
+                                            
+                                            <form action="/events/{{ $meeting_event->id }}" id="form_delete" method="post">
+                                                {{ csrf_field() }}
+                                                @method('DELETE')
+                                                <button type="button" class="btn btn-secondary" onclick="deleteSchedule(this);">削除</button> 
+                                            </form>
+                                            
+                                            <a href="/events/{{$meeting_event->id}}">
+                                                <button type="button" class="btn btn-secondary">編集</button>
+                                            </a>
+                                        </li>
+                                        
+                                    @endif
+                                @endforeach
                             @endif
-                        @endforeach
+                            
+                        </ul>
                     </div>
+                    
+                    <div class="col-md-1"></div>
                 </div>
             </div>
             
@@ -47,14 +87,16 @@
             </div>
             
             <div class="col-md-5" id="month_view">
-                
+                <!-- 月単位で予定を表示する部分 -->
             </div>
             
             <div class="col-md-1">
             </div>
+            
         </div>
     </div>
     
+    <!-- モーダル表示部分 -->
     <div class="modal modal-background">
         <div class="modal-content">
             <div class="modal-header">
@@ -66,11 +108,19 @@
           　    <div class="modal-body-title"></div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary modal-close" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary modal-close" data-dismiss="modal">閉じる</button>
             </div>
         </div>
     </div>
 
     <script src="{{ mix('js/Calendar/MonthView.js') }}"></script>
+    
+    <script>
+            function deleteSchedule(e){
+                if (confirm('削除すると復元が出来ません。\n本当に削除しますか？')){
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
 
 @endsection
