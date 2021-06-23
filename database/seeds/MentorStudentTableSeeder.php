0bb6b3567ff273cc5aacdd4a2297f45bfef03264
bwkw <na.shota.1139@gmail.com>
2021-06-23 22:06:32 +0900

    変数名一部変更

--- database/seeds/MentorStudentTableSeeder.php
+++ database/seeds/MentorStudentTableSeeder.php
@@ -8,7 +8,7 @@ class MentorStudentTableSeeder extends Seeder
     // mentors table（メンター情報格納テーブル）とstudents table（生徒情報格納テーブル）に格納
      public function run()
     {
-        // Slack APIを叩く（curlを利用）
+        // Slack APIを叩いてslackチームのユーザーリストを取得
         $headers = [
             'Authorization: Bearer '.config('app.slack_member_list'),
             'Content-Type: application/json;charser¥t=ytf-8'
@@ -34,25 +34,25 @@ class MentorStudentTableSeeder extends Seeder
         // curlでAPIを呼び出し、返ってきたjsonデータを日本語にする
         $unicode_decode_json = json_encode(json_decode($result), JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
         
-        //正規表現でid、display_name(slackで表示されている名前)を取得する
+        // 正規表現でid、display_name(slackで表示されている名前)を取得
         preg_match_all('/"id": "(?P<slack_id>[A-Z0-9]+)"/', $unicode_decode_json, $slack_id_match);
         preg_match_all('/"display_name": "(?P<slack_name>.*)"/', $unicode_decode_json, $slack_name_match);
         
-        //空配列を用意し、その空配列にslack_id、slack_nameを格納していく
+        // 空配列を用意し、その空配列にslack_id、slack_nameを格納
         $slack_id_list = [];
         array_push($slack_id_list, $slack_id_match["slack_id"]);
         $slack_name_list = [];
         array_push($slack_name_list, $slack_name_match["slack_name"]);
         
-        //slackチームに入っている人のid,slacknameを連想配列にする
+        // slackチームのユーザーのid,slacknameを連想配列に
         $slack_id_name_list = array_combine($slack_id_list[0], $slack_name_list[0]);
         
-        //全体の連想配列からメンター、生徒の連想配列に分解
+        // 全体の連想配列からメンター、生徒の連想配列に分解
         $mentor_slack_id_name_list = [];
         $student_slack_id_name_list = [];
         foreach ($slack_id_name_list as $slack_id => $slack_name) {
             if(strstr($slack_name,'メンター')){
-                // 特定の文字（メンター）を消し、名前だけを抽出する
+                // 特定の文字（メンター）を消し、名前だけを抽出
                 $slack_name = str_replace('(メンター)', '', $slack_name);
                 $slack_name = str_replace('（メンター）', '', $slack_name);
                 $mentor_slack_id_name_list[$slack_id] = $slack_name;
