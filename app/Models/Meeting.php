9e6fb8893e02722adf0a7f841ccd66634b7cd75b
bwkw <na.shota.1139@gmail.com>
2021-06-29 19:27:31 +0900

    にhow_to追加

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -10,6 +10,7 @@ class Meeting extends Model
     protected $fillable = [
         'mentor_name',
         'student_name',
+        'how_to'
         'date',
         'beginning_time',
         'ending_time',
