605e777882377c70cd09e0aa8eaade4f2598e14b
bwkw <na.shota.1139@gmail.com>
2021-06-30 22:00:57 +0900

    slackからの情報取得方法の変更

--- database/seeds/MentorStudentTableSeeder.php
+++ database/seeds/MentorStudentTableSeeder.php
@@ -34,30 +34,35 @@ class MentorStudentTableSeeder extends Seeder
         // curlでAPIを呼び出し、返ってきたjsonデータを日本語にする
         $unicode_decode_json = json_encode(json_decode($result), JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
         
-        // 正規表現でid、display_name(slackで表示されている名前)を取得
+        // 正規表現でid、real_name(slackで表示されている名前)を取得
         preg_match_all('/"id": "(?P<slack_id>[A-Z0-9]+)"/', $unicode_decode_json, $slack_id_match);
-        preg_match_all('/"display_name": "(?P<slack_name>.*)"/', $unicode_decode_json, $slack_name_match);
+        preg_match_all('/"skype": "",\s*"real_name": "(?P<real_name>.*)"/', $unicode_decode_json, $real_name_match);
+        preg_match_all('/"display_name": "(?P<slack_display_name>.*)"/', $unicode_decode_json, $slack_display_name_match);
         
-        // 空配列を用意し、その空配列にslack_id、slack_nameを格納
-        $slack_id_list = [];
-        array_push($slack_id_list, $slack_id_match["slack_id"]);
-        $slack_name_list = [];
-        array_push($slack_name_list, $slack_name_match["slack_name"]);
-        
-        // slackチームのユーザーのid,slacknameを連想配列に
-        $slack_id_name_list = array_combine($slack_id_list[0], $slack_name_list[0]);
+        $slack_id_name_list = [];
+        for ($i = 0; $i < (count($slack_id_match["slack_id"])); $i++) {
+            $slack_id_match_item = $slack_id_match["slack_id"][$i];
+            $real_name_match_item = $real_name_match["real_name"][$i];
+            $slack_display_name_match_item = $slack_display_name_match["slack_display_name"][$i];
+            if(($slack_display_name_match_item)==""){
+                $slack_id_name_list[$slack_id_match_item] = $real_name_match_item;
+            }
+            else{
+                $slack_id_name_list[$slack_id_match_item] = $slack_display_name_match_item;
+            }
+        }
         
         // 全体の連想配列からメンター、生徒の連想配列に分解
         $mentor_slack_id_name_list = [];
         $student_slack_id_name_list = [];
         foreach ($slack_id_name_list as $slack_id => $slack_name) {
-            if(strstr($slack_name,'メンター')){
+            if((strstr($slack_name,'(メンター)')) or (strstr($slack_name,'（メンター）'))){
                 // 特定の文字（メンター）を消し、名前だけを抽出
                 $slack_name = str_replace('(メンター)', '', $slack_name);
                 $slack_name = str_replace('（メンター）', '', $slack_name);
                 $mentor_slack_id_name_list[$slack_id] = $slack_name;
             }
-            elseif((strstr($slack_name,'メンター')==false) and ($slack_name!="") and ($slack_name!="Slackbot")){
+            elseif((strstr($slack_name,'（')==false) and (strstr($slack_name,'(')==false) and ($slack_name!="") and ($slack_name!="Slackbot")){
                 $student_slack_id_name_list[$slack_id] = $slack_name;
             }
         }
