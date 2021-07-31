9fb635d4af915bd985555baf3550f49cdf9bbb86
bwkw <na.shota.1139@gmail.com>
2021-07-31 17:06:00 +0900

    fillableをguardedに変更

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -9,14 +9,9 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Meeting extends Model
 {
-    // 指定したカラムに対してfillが出来るように$fillable定義
-    protected $fillable = [
-        'mentor_name',
-        'student_name',
-        'how_to',
-        'date',
-        'beginning_time',
-        'ending_time',
-        'user_id',
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
     ];
 }
