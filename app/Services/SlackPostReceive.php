d9959cf8dd22cb2dfe126922b92b5d5bb90ffb49
bwkw <na.shota.1139@gmail.com>
2021-07-31 17:46:48 +0900

    Slacにメッセージ送信用メソッド定義

--- app/Services/SlackPostReceive.php
+++ app/Services/SlackPostReceive.php
@@ -13,6 +13,85 @@ use App\Services\ProcessDate;
 class SlackPostReceive
 {
     /**
+     * Slackに送信するテキストを作成
+     */
+    public function createSlackText(ProcessDate $date, Meeting $meeting, Mentor $mentor, Student $student)
+    {
+        $mentors = $mentor->get();
+        
+        // 本日の曜日を取得
+        $nowDayOfTheWeek = $date->dayOfTheWeek(date('w'));
+        
+        $text = "*" . date('m/d') . "（" . $nowDayOfTheWeek . "）" . "の面談予定*\n";
+        
+        // 面談があるかを判別する変数
+        $todayMeetingsExist = false;
+        
+        foreach ($mentors as $mentor) {
+            $mentorName = $mentor->slack_name;
+            
+            // 本日の面談を取得
+            $todayMeetings = $meeting->getTodayMeetings($mentorName);
+            if (count($todayMeetings) > 0) {
+                $todayMeetingsExist = true;
+                
+                // 面談者ありの場合、slackに送るテキスト
+                $text .= "\n■" . $mentorName . "との面談者\n";
+                foreach ($todayMeetings as $todayMeeting) {
+                    $studentName = $todayMeeting->student_name;
+                    $studentSlackId = $student->getStudentData($studentName)->slack_id;
+                    $howToMeeting = $todayMeeting->how_to;
+                    $beginningTime = $todayMeeting->beginning_time;
+                    $endingTime = $todayMeeting->ending_time;
+                    $beginningTime = date("H:i", strtotime($beginningTime));
+                    $endingTime = date("H:i", strtotime($endingTime));
+                    $text .= "<@" . $studentSlackId . ">\t" . $beginningTime . "〜" . $endingTime . "\t" . $howToMeeting . "\n";
+                }
+            }
+        }
+        
+        // 面談者なしの場合、slackに送るテキスト
+        if (!$todayMeetingsExist){
+            $text .= "\n本日の面談予定はありません";
+        }
+        
+        return $text;
+    }
+    
+    /**
+     * Slackにメッセージを送信する
+     */
+    public function postSlackMessage($text)
+    {
+        // Slack APIを叩いてslackチームにメッセージを送信
+        $headers = [
+            'Authorization: Bearer ' . config('app.slack_post_message'),
+            'Content-Type: application/json;charser¥t=ytf-8'
+        ];
+        
+        $url = "https://slack.com/api/chat.postMessage";
+        
+        $send_fields = [
+            "channel" => "面談リマインド",
+            "text" => $text
+        ];
+        
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => json_encode($send_fields)
+        ];
+        
+        // 実行処理
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        $result = curl_exec($ch); 
+        curl_close($ch);
+    }
+    
+    /**
      * Slackチームのメンバーリストを連想配列で取得する
      */
     public function fetchSlackMemberlist()
