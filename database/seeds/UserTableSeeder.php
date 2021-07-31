7e818e9f0ee9f098849c174b4b436074ae8cbac4
bwkw <na.shota.1139@gmail.com>
2021-07-31 12:07:28 +0900

    クラスのPHPDoc記載

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,21 +2,23 @@
 
 use Illuminate\Database\Seeder;
 
+/**
+* mentorsテーブルの情報取得・usersテーブルに情報格納用シーダークラス
+*/
 class UserTableSeeder extends Seeder
 {
-    
     public function run()
     {
         // usersテーブルの情報を削除
         DB::table('users')->truncate();
         
-        // usersテーブルに値を格納
+        // usersテーブルに情報を格納
         $mentors = DB::table('mentors')->get();
-        foreach($mentors as $mentor)
+        foreach ($mentors as $mentor) {
             DB::table('users')->insert(
-                    ['name' => $mentor->slack_name,
-                     'password' => Hash::make(config('app.users_password'))]
+                ['name' => $mentor->slack_name,
+                 'password' => Hash::make(config('app.users_password'))]
             );
-        
+        }
     }
 }
