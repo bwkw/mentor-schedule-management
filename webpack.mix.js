149764e93ccd41492a869ad9a412a469f3590184
bwkw <na.shota.1139@gmail.com>
2021-08-02 00:07:18 +0900

    ExampleComponent.vue削除に伴い不要なコードを削除

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .js('resources/js/Calendar/MonthView.js', 'public/js/Calendar')
-   .js('resources/js/Calendar/TimeGridView.js', 'public/js/Calendar')
-   .js('resources/js/Calendar/FetchMeetingSchedule.js', 'public/js/Calendar')
-   .js('resources/js/Calendar/FetchEventSchedule.js', 'public/js/Calendar')
+   .js('resources/js/Calendar/fetch-event-schedule.js', 'public/js/Calendar')
+   .js('resources/js/Calendar/fetch-meeting-schedule.js', 'public/js/Calendar')
+   .js('resources/js/Calendar/month-view.js', 'public/js/Calendar')
+   .js('resources/js/Calendar/time-grid-view.js', 'public/js/Calendar')
    .sass('resources/sass/app.scss', 'public/css');
