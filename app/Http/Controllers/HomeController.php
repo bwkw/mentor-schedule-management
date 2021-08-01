c7f6629d1594624a4030309dd4f6a4f7c2218810
bwkw <na.shota.1139@gmail.com>
2021-08-01 23:37:55 +0900

    変数名をcamelCaseに・処理方法の変更

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,37 +19,32 @@ class HomeController extends Controller
     public function index()
     {
         // 現在の日時を取得
-        $now_date = date('Y-m-d');
-        $now_time = date('H:i:s');
+        $nowDate = date('Y-m-d');
+        $nowTime = date('H:i:s');
         
         // 「現在の日付より前」かつ、「現在の時刻より終了時間が前」の情報をmeetings,eventsテーブルから削除
-        Meeting::where('date', '<=', $now_date) -> where('ending_time', '<=', $now_time) -> delete();
-        Event::where('date', '<=', $now_date) -> where('ending_time', '<=', $now_time) -> delete();
+        Meeting::where('date', '<=', $nowDate)->where('ending_time', '<=', $nowTime)->delete();
+        Event::where('date', '<=', $nowDate)->where('ending_time', '<=', $nowTime)->delete();
 
         // ユーザー情報に基づいて、meetingsテーブルとeventsテーブルから情報を取得
         $meetings = User::find(Auth::user()->id)->meetings;
         $events = User::find(Auth::user()->id)->events;
         
-        // 二つのオブジェクトを結合する
-        $meetings_events = (object)array_merge_recursive((array) $meetings, (array) $events);
+        // それぞれのオブジェクトを連想配列にし、結合する
+        $meetings = json_decode($meetings, true);
+        $events = json_decode($events, true);
+        $meetingsEvents = array_merge($meetings, $events);
         
-        // オブジェクトを配列に
-        $meetings_events_array = "";
-        foreach($meetings_events as $meeting_event)
-        {
-            $meetings_events_array = $meeting_event;
-        }
+        // 連想配列からdate(日付)、starting_time（開始時間)、ending_time(終了時間)を取得する
+        $dates = array_column($meetingsEvents, 'date');
+        $beginningTimes = array_column($meetingsEvents, 'beginning_time');
+        $endingTimes = array_column($meetingsEvents, 'ending_time');
         
-        // 配列からdate(日付)、starting_time（開始時間)、ending_time(終了時間)を取得する
-        $dates = array_column($meetings_events_array, 'date');
-        $beginning_times = array_column($meetings_events_array, 'beginning_time');
-        $ending_times = array_column($meetings_events_array, 'ending_time');
-        
-        // 取得したdate(日付)、starting_time（開始時間)、ending_time(終了時間)を基に、配列を並び替える
+        // 取得したdate(日付)、starting_time（開始時間)、ending_time(終了時間)を基に、連想配列を並び替える
         array_multisort(array_map("strtotime", $dates), SORT_ASC,
-                        array_map("strtotime", $beginning_times), SORT_ASC,
-                        array_map("strtotime", $ending_times), SORT_ASC, $meetings_events_array);
-
-        return view('Home.home') -> with(['meetings_events_array' => $meetings_events_array]);
+                        array_map("strtotime", $beginningTimes), SORT_ASC,
+                        array_map("strtotime", $endingTimes), SORT_ASC, $meetingsEvents);
+        
+        return view('Home.home')->with(['meetingsEvents' => $meetingsEvents]);
     }
 }
