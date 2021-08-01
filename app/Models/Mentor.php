041c709765a56084ff5d86ca6ead306c1fa7f675
bwkw <na.shota.1139@gmail.com>
2021-08-01 18:19:05 +0900

    クラスのPHPDoc記載・guarded追加

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -4,7 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * メンター用モデルクラス
+ */
 class Mentor extends Model
 {
-    
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
 }
