2d8e8185ca3e492fc15aade9ea53ce3629c071f9
bwkw <na.shota.1139@gmail.com>
2021-08-02 17:02:38 +0900

    chosen-select.js追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,5 +16,6 @@ mix.js('resources/js/app.js', 'public/js')
    .js('resources/js/Calendar/fetch-meeting-schedule.js', 'public/js/Calendar')
    .js('resources/js/Calendar/month-view.js', 'public/js/Calendar')
    .js('resources/js/Calendar/time-grid-view.js', 'public/js/Calendar')
+   .js('resources/js/chosen-select.js', 'public/js')
    .js('resources/js/delete-check.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
