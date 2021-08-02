952501038ba47de3cec675d259c0456223a6a4e5
bwkw <na.shota.1139@gmail.com>
2021-08-02 16:27:57 +0900

    link/scriptタグをソート・yield追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,20 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <script src="{{ asset('js/app.js') }}"></script>
-    
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('link')
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('script')
+    
+    <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 <body>
     <div id="app">
