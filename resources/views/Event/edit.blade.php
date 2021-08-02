d31c47c2e1cd15e4ba86338bd615c1a9fc35959e
bwkw <na.shota.1139@gmail.com>
2021-08-02 17:37:18 +0900

    link/scriptタグをsectionに

--- resources/views/Event/edit.blade.php
+++ resources/views/Event/edit.blade.php
@@ -1,64 +1,71 @@
 @extends('layouts.app')
 
-@section('content')
-<link rel='stylesheet' href="{{asset('css/modal.css')}}">
+@section('link')
+    <link rel='stylesheet' href="{{ asset('css/modal.css') }}">
+@endsection
 
-    <div class="mt-5">
-        <div class="row">
-            <div class="col-md-1">
-            </div>
-            
-            <div class="col-md-4 border">
-                <br>
-                <h2>イベント予約</h2>
-                <br>
-                <div class="row">
-                    <div class="col-md">
-                        <form action="/events/{{ $event->id }}" method="POST">
-                            {{ csrf_field() }}
-                            @method('PUT')
-                            <div class="form-group mb-4">
-                                <label>メンター氏名(slack)：</label>
-                                <input type="input" class="form-control" value="{{ $event->mentor_name }}" readonly>
-                            </div>
-                            <div class="form-group mb-4">
-                                <label for="date" class="col-form-label">イベント名</label>
-                                <input type="input" class="form-control" value="{{ $event->event_name }}" readonly>
-                            </div>
-                            <div class="form-group mb-4">
-                                <label for="date" class="col-form-label">日程</label>
-                                <input type="date" class="form-control" name="event[date]" id="date" value="{{ $event->date }}">
-                            </div>
-                            <div class="form-group mb-4">
-                                <label for="beginning_time" class="col-form-label">開始時間</label>
-                                <input type="time" class="form-control" name="event[beginning_time]" id="beginning_time" value="{{ $event->beginning_time }}">
-                            </div>
-                            <div class="form-group mb-4">
-                                <label for="ending_time" class="col-form-label">終了時間</label>
-                                <input type="time" class="form-control" name="event[ending_time]" id="ending_time" value="{{ $event->ending_time }}">
-                            </div>
-                            
-                            <!-- ユーザーidをリレーション用に格納 -->
-                            <input type="hidden" name="event[user_id]" value="{{ Auth::user()->id }}">
-                            
-                            <div class="text-center mt-3 mb-2">
-                                <button type="submit" class="btn btn-secondary">送信</button>
-                            </div>
-                        </form>
-                    </div>
+@section('script')
+    <script src="{{ mix('js/Calendar/time-grid-view.js') }}"></script>
+@endsection
+
+@section('content')
+
+    <div class="mt-5 row">
+        
+        <div class="col-md-1">
+        </div>
+        
+        <div class="col-md-4 border">
+            <br>
+            <h2>イベント予約</h2>
+            <br>
+            <div class="row">
+                <div class="col-md">
+                    <form action="/events/{{ $event->id }}" method="POST">
+                        {{ csrf_field() }}
+                        @method('PUT')
+                        <div class="form-group mb-4">
+                            <label>メンター氏名(slack)</label>
+                            <input type="input" class="form-control" value="{{ $event->mentor_name }}" readonly>
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="date" class="col-form-label">イベント名</label>
+                            <input type="input" class="form-control" value="{{ $event->event_name }}" readonly>
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="date" class="col-form-label">日付</label>
+                            <input type="date" class="form-control" name="event[date]" value="{{ $event->date }}">
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="beginning_time" class="col-form-label">開始時間</label>
+                            <input type="time" class="form-control" name="event[beginning_time]" value="{{ $event->beginning_time }}">
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="ending_time" class="col-form-label">終了時間</label>
+                            <input type="time" class="form-control" name="event[ending_time]" value="{{ $event->ending_time }}">
+                        </div>
+                        
+                        <!-- ユーザーidをリレーション用に格納 -->
+                        <input type="hidden" name="event[user_id]" value="{{ Auth::user()->id }}">
+                        
+                        <div class="text-center mt-3 mb-2">
+                            <button type="submit" class="btn btn-secondary">送信</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-            
-            <div class="col-md-1">
-            </div>
-              
-            <div class="col-md-5" id="time_grid_view">
-                    
-            </div>
-            
-            <div class="col-md-1">
-            </div>
         </div>
+        
+        <div class="col-md-1">
+        </div>
+          
+        <div class="col-md-5" id="time_grid_view">
+                
+        </div>
+        
+        <div class="col-md-1">
+        </div>
+    </div>
     
     <div class="modal modal-background">
         <div class="modal-content">
@@ -82,9 +89,5 @@
             <button type="button" class="btn btn-secondary">戻る</button>
         </a>
     </div>
-    
-    <script src="{{ mix('js/Calendar/TimeGridView.js') }}"></script>
-    <script src="{{ mix('js/Calendar/FetchMeetingSchedule.js') }}"></script>
-    <script src="{{ mix('js/Calendar/FetchEventSchedule.js') }}"></script>
     
 @endsection
